59a7e6967fb7b342ce8b539a8f730dde393e25fc
WanChengXue <WanChengXue@users.noreply.github.com>
2024-08-27 07:54:10 +0000

    ✅ Ready to clone and code.

---
+++ rl_framework/__main__.py
@@ -0,0 +1,6 @@
+"""Entry point for rl_framework."""
+
+from rl_framework.cli import main  # pragma: no cover
+
+if __name__ == "__main__":  # pragma: no cover
+    main()
--- tests/test_base.py
+++ tests/test_base.py
@@ -1,5 +1,5 @@
-from project_name.base import NAME
+from rl_framework.base import NAME
 
 
 def test_base():
-    assert NAME == "project_name"
+    assert NAME == "rl_framework"
